6e630fa9e198157425009409171c1ca9adeb6e6d
DarkCodeStudios <164196645+DarkCodeStudios@users.noreply.github.com>
2024-08-31 15:04:23 +0200

    Print Function
    Un modo semplice per stampare i tuoi dati come Nome/Cognome/Età sul tuo   terminale o sulla console del browser

---
+++ simple_information__print_in_javascript.js
@@ -0,0 +1,11 @@
+/* Simple javascript function for print your informations in the terminal/web console*/
+
+const Name = "YOUR_NAME"; 
+const Surname = "YOUR_SURNAME";
+const Age = 23;
+
+const print_name = "Name: " + Name;
+const print_surname = " Surname: " + Surname;
+const print_age = " Age: " + Age;
+
+console.log(print_name + print_surname + print_age)
